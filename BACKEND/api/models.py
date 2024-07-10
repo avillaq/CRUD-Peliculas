b2765bbca23a13b00fda8f5b8748a46b16278627
avillaq <villafuertequispealex@gmail.com>
2024-07-10 13:19:19 -0500

    Creamos un modelo para los generos

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -6,8 +6,14 @@ class Movie(models.Model):
     description = models.TextField()
     image = models.URLField(max_length=200)
     rating = models.IntegerField()
-    genre = models.CharField(max_length=50)
+    genres = models.ManyToManyField("Genre")
     inTheaters = models.BooleanField()
 
+    def __str__(self):
+        return self.name
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    
     def __str__(self):
         return self.name
