4a29e88ff491f00fa20bb6922c02a19cd464a22e
avillaq <villafuertequispealex@gmail.com>
2024-07-10 12:59:45 -0500

    Modelo movie creado

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(max_length=200)
+    rating = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    inTheaters = models.BooleanField()
+
+    def __str__(self):
+        return self.name
