aff3a2ad652101c34b950251dc55401051a6ba38
avillaq <villafuertequispealex@gmail.com>
2024-07-10 23:56:21 -0500

    Diseño formulario para crear terminado

--- FRONTEND/src/app/crear/crear.component.ts
+++ FRONTEND/src/app/crear/crear.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {FormBuilder, Validators, ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-crear',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './crear.component.html',
   styleUrl: './crear.component.css'
 })
-export class CrearComponent {
+export class CrearComponent implements OnInit {
+  formulario: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    description : new FormControl(''),
+    url_image : new FormControl(''),
+    ranking : new FormControl(''),
+    genres : new FormControl(''),
+    inTheaters : new FormControl(''),
+  });
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      url_image: ['', Validators.required],
+      ranking: ['', Validators.required],
+      genres: ['', Validators.required],
+      inTheaters: ['', Validators.required],
+    });
+    
+  }
+
+  onSubmit(): void {
+    console.log(this.formulario.value);
+    
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+      
+      alert("Boletos Comprados Exitosamente!");
+    } else {
+      alert("Por favor, llena todos los campos");
+    }
+  }
+
+  
 }
