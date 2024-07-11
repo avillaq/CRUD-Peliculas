e824ff470e944d58e6e4fb4052002fbcff75b355
avillaq <villafuertequispealex@gmail.com>
2024-07-10 21:46:24 -0500

    Añadimos un footer

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './navegacion/navegacion.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegacionComponent],
+  imports: [RouterOutlet, NavegacionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
