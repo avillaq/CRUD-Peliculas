56c81f7bb44f3ad3808e8c6995ec0532329abde4
avillaq <villafuertequispealex@gmail.com>
2024-07-10 20:39:54 -0500

    Añadimos rutas de navegacion al frontend

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { ListarComponent } from './listar/listar.component';
+import { CrearComponent } from './crear/crear.component';
+import { EditarComponent } from './editar/editar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: PeliculasComponent },
+    { path: "administrar", component: ListarComponent},
+    { path: "administrar/crear/:id_pelicula", component: CrearComponent},
+    { path: "administrar/editar/:id_pelicula", component: EditarComponent},
+];
