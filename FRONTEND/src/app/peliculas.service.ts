2a88d00948292bd2893ec013419165d23f4277d2
avillaq <villafuertequispealex@gmail.com>
2024-07-11 00:03:19 -0500

    Creamos el servicio peliculas  que se conecta a la api del backend

--- FRONTEND/src/app/peliculas.service.ts
+++ FRONTEND/src/app/peliculas.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
+  constructor( private http: HttpClient ) {}
 
-  constructor() { }
+  getPeliculas() {
+    return this.http.get('http://127.0.0.1:8000/api/movies/')
+  }
+  getPelicula(id: number) {
+    return this.http.get("http://127.0.0.1:8000/api/movies/" + id + "/") 
+  }
+  crearPelicula(pelicula: any) {
+    return this.http.post("http://127.0.0.1:8000/api/movies/", pelicula);
+  }
 }
